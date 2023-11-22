67820a3b38d3146a4f4a5d1e035c5a61b6148b22
Tobi-3 <47795799+Tobi-3@users.noreply.github.com>
2023-11-22 11:44:08 +0100

    submission for part3

---
+++ backend/index.js
@@ -0,0 +1,137 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.data(req, res)
+  ].join(' ')
+}))
+
+
+morgan.token('data', (req, _res) => {
+  return JSON.stringify(req.body)
+})
+
+const unknownEndpoint = (_req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// eslint-disable-next-line no-unused-vars
+const persons = [
+  {
+    'id': 1,
+    'name': 'Arto Hellas',
+    'number': '040-123456'
+  },
+  {
+    'id': 2,
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
+  },
+  {
+    'id': 3,
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
+  },
+  {
+    'id': 4,
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
+  }
+]
+
+app.get('/api/persons', (_req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+app.get('/api/infos', (_req, res, next) => {
+  Person.countDocuments({})
+    .then(count => res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`))
+    .catch(error => next(error))
+
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(_result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  }).catch(error => next(error))
+
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, _req, res, _next) => {
+  console.error(error)
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  } else {
+    return res.status(400).json({ error: error.message })
+  }
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
